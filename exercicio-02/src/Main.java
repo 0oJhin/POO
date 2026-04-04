68fd2e6d55a77ce8c260c8f4697b636627a089a6
Pedro Henrique <ph700640@gmail.com>
2026-04-04 16:26:45 -0300

    Create Main.java

---
+++ exercicio-02/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pess1 = new Pessoa();
+        pess1.setIdade(20);
+        pess1.setNome("Ana");
+        pess1.mostrarDados();
+    }
+}
