b5a2fae8ec32db9775d843d5b03910e1000e60d3
Pedro Henrique <ph700640@gmail.com>
2026-04-04 16:31:31 -0300

    Create Main.java

---
+++ exercicio04/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Carro katial = new Carro("Celta");
+        Pessoa Amilton = new Pessoa("Amilton");
+
+        Amilton.usarCarro(katial);
+    }
+}
