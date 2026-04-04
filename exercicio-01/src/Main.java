e0d0fe574e61f1cd7cdec785dd90098499654014
Pedro Henrique <ph700640@gmail.com>
2026-04-04 16:20:28 -0300

    exercício01

---
+++ exercicio-01/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // implementar aqui a regra de negocio
+        Gato gato1 = new Gato("Sushi", 9 , true ,5);
+        Gato gato2 = new Gato("Cachaca", 5 , false, 6);
+        gato1.comer();
+        gato2.comer();
+        gato1.dormir();
+        gato2.dormir();
+        gato1.comer();
+        gato2.comer();
+        gato2.dormir();
+        gato1.miarrr();
+        gato1.miarrr();
+        gato1.miarrr();
+        gato1.miarrr();
+        gato1.miarrr();
+        gato1.miarrr();
+        gato1.dormir();
+        gato1.miarrr();
+    }
+}
