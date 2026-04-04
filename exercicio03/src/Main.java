b77babe091ecb9d41bee4c734c687a4c3be664ff
Pedro Henrique <ph700640@gmail.com>
2026-04-04 16:27:52 -0300

    Create Main.java

---
+++ exercicio03/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Flor fro1 = new Flor();
+        fro1.setCor("Amarelo");
+        fro1.setNome("girassol");
+        fro1.setPerfume(true);
+        fro1.mostrarDados();
+    }
+}
