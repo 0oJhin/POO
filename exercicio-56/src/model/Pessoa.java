3593626f72c7071809fb2bc041bf9415e8436d05
Pedro Henrique <ph700640@gmail.com>
2026-06-12 18:35:51 -0300

    Exercicio-56
    CRUD funcional

---
+++ exercicio-56/src/model/Pessoa.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Pessoa {
+    private int id;
+    private String nome;
+
+    public Pessoa(){
+    }
+    public Pessoa(String nome){
+        this.nome = nome;
+    }
+    public Pessoa(int id, String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
